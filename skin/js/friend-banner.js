2441170512b33fc8a8b08c3775920333defd5613
Apple <sos6723@naver.com>
2026-06-18 11:32:02 +0900

    Update friend-banner.js

--- skin/js/friend-banner.js
+++ skin/js/friend-banner.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
         // 새로 정하신 파일명인 friend_banner.html을 로드합니다.
-        fetch('./suna-star/pages/friend-banner.html')
+        fetch('.pages/friend-banner.html')
             .then(response => response.text())
             .then(data => {
                 const containers = document.querySelectorAll('.banner-module.module.friend_banner');
